67eb1ae2a4f246421abf53b3642aec41b9296aea
Neeraj Kashyap <nkashy1@gmail.com>
2021-02-24 14:57:11 -0800

    Added tests for the Reporter class
    These tests are still minimal. They check that reports are successfully
    generated.

---
+++ python/flypaper/test_report.py
@@ -0,0 +1,21 @@
+from os import system
+import unittest
+
+from . import consent, report
+
+
+class TestReporter(unittest.TestCase):
+    def setUp(self):
+        self.consent = consent.FlypaperConsent(True)
+        self.reporter = report.Reporter("TestReporter", self.consent)
+
+    def test_system_report_successful(self):
+        self.reporter.system_report(publish=False)
+
+    def test_error_report_successful(self):
+        error = Exception("This exception is for use in a Flypaper Python test")
+        self.reporter.error_report(error, publish=False)
+
+
+if __name__ == "__main__":
+    unittest.main()
