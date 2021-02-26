2d576810d6be56958eee5811550c46ab7df9b974
Neeraj Kashyap <nkashy1@gmail.com>
2021-02-26 05:14:49 -0800

    Changed names in setup.py

--- python/setup.py
+++ python/setup.py
@@ -5,7 +5,7 @@ with open("README.md") as ifp:
     long_description = ifp.read()
 
 setup(
-    name="flypaper",
+    name="humbug",
     version="0.1.1",
     packages=find_packages(),
     install_requires=["bugout"],
@@ -13,7 +13,7 @@ setup(
         "dev": ["black", "mypy", "wheel"],
         "distribute": ["setuptools", "twine", "wheel"],
     },
-    description="Flypaper: Do you build developer tools? Flypaper helps you know your users.",
+    description="Humbug: Do you build developer tools? Humbug helps you know your users.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="Bugout.dev",
@@ -24,5 +24,5 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Topic :: Software Development :: Libraries",
     ],
-    url="https://github.com/bugout-dev/flypaper",
+    url="https://github.com/bugout-dev/humbug",
 )
