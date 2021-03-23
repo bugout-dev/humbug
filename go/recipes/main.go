d8c917076dd9332306b90e8c5114b727a8c79d70
Neeraj Kashyap <nkashy1@gmail.com>
2021-03-23 06:22:12 -0700

    Minimally working version of humbug Go

--- go/recipes/main.go
+++ go/recipes/main.go
@@ -16,12 +16,21 @@ func main() {
 	bugoutJournalID := os.Getenv("BUGOUT_JOURNAL_ID")
 
 	reporter, _ := humbug.CreateHumbugReporter(consent, clientID, sessionID, bugoutAccessToken, bugoutJournalID)
+	// RECIPE: This is how you record panics from your go routines.
+	defer func() {
+		message := recover()
+		if message != nil {
+			report := humbug.PanicReport(message)
+			reporter.Publish(report)
+			// recovery code goes here
+		}
+	}()
 
-	report := humbug.Report{
-		Title:   "test",
-		Tags:    []string{"test"},
-		Content: "This is a test",
-	}
+	// RECIPE: This is how you record system information
+	report := humbug.SystemReport()
 
 	reporter.Publish(report)
+
+	// And now we panic to show how the panic is reported.
+	panic("OH NOES!!!")
 }
