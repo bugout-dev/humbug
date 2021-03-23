c73641f5007dbfb29de2aeea75627ee3dbfbb7f3
Neeraj Kashyap <nkashy1@gmail.com>
2021-03-23 04:52:12 -0700

    Basic publication logic for HumbugReporter

--- go/pkg/report.go
+++ go/pkg/report.go
@@ -1,9 +1,8 @@
 package humbug
 
 import (
-	"time"
-
 	bugout "github.com/bugout-dev/bugout-go/pkg"
+	"github.com/bugout-dev/bugout-go/pkg/spire"
 )
 
 type Report struct {
@@ -15,7 +14,6 @@ type Report struct {
 type Reporter interface {
 	Tags() []string
 	Publish(Report) error
-	SetTimeout(time.Duration)
 }
 
 type HumbugReporter struct {
@@ -25,9 +23,44 @@ type HumbugReporter struct {
 	bugoutClient      bugout.BugoutClient
 	bugoutAccessToken string
 	bugoutJournalID   string
-	tags              []string
+	tags              map[string]bool
 }
 
-func (reporter HumbugReporter) Tags() []string {
-	return []string{}
+func (reporter *HumbugReporter) Tags() []string {
+	tags := make([]string, len(reporter.tags))
+	i := 0
+	for tag, value := range reporter.tags {
+		if value {
+			tags[i] = tag
+			i++
+		}
+	}
+	return tags[:i]
+}
+
+func MergeTags(tags0, tags1 []string) []string {
+	tagSet := make(map[string]bool)
+	for _, tag := range tags0 {
+		tagSet[tag] = true
+	}
+	for _, tag := range tags1 {
+		tagSet[tag] = true
+	}
+	mergedTags := make([]string, len(tagSet))
+	i := 0
+	for tag := range tagSet {
+		mergedTags[i] = tag
+		i++
+	}
+	return mergedTags
+}
+
+func (reporter *HumbugReporter) Publish(report Report) error {
+	context := spire.EntryContext{
+		ContextType: "humbug",
+		ContextID:   reporter.sessionID,
+	}
+	tags := MergeTags(report.Tags, reporter.Tags())
+	_, err := reporter.bugoutClient.Spire.CreateEntry(reporter.bugoutAccessToken, reporter.bugoutJournalID, report.Title, report.Content, tags, context)
+	return err
 }
