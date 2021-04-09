bf320b41df0cc35356b0d48c65395653f0d79ea8
kompotkot <zenitsws@gmail.com>
2021-04-09 11:25:13 +0000

    Raw implementation of js module

---
+++ javascript/src/consent.ts
@@ -0,0 +1,7 @@
+/*
+This module implements Humbug's user consent mechanisms.
+*/
+
+type Yes = "1" | "t" | "y" | "T" | "Y" | "true" | "yes" | "True" | "Yes" | "TRUE" | "YES"
+type No = "0" | "f" | "n" | "F" | "N" | "false" | "no" | "False" | "No" | "FALSE" | "NO"
+
