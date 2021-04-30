689b9e977c49062bbc71e72845a0d30cf8e7b9c9
kompotkot <zenitsws@gmail.com>
2021-04-30 10:36:02 +0000

    Simplified error tagging

--- python/humbug/report.py
+++ python/humbug/report.py
@@ -87,20 +87,6 @@ class HumbugReporter:
         self.is_excepthook_set = False
         self.is_loggerhook_set = False
 
-    def _extract_error_base(self, error: Exception) -> List[str]:
-        error_bases_list = []
-        try:
-            for error_base in error.__class__.__bases__:
-                error_base_module = error_base.__dict__.get("__module__", None)
-                if error_base_module is not None:
-                    error_name = "{}.{}".format(error_base_module, error_base.__name__)
-                else:
-                    error_name = error_base.__name__
-                error_bases_list.append("{}".format(error_name))
-        except Exception:
-            pass
-        return error_bases_list
-
     def wait(self) -> None:
         concurrent.futures.wait(
             self.report_futures, timeout=float(self.timeout_seconds)
@@ -251,12 +237,13 @@ Release: `{os_release}`
         )
         if tags is None:
             tags = []
-
-        error_bases = self._extract_error_base(error=error)
-        for error_base in error_bases:
-            tags.append("error:{}".format(error_base))
-
-        tags.append("type:error")
+        tags.extend(
+            [
+                "type:error",
+                "error:{}".format(error.__class__.__name__),
+                "error:{}.{}".format(error.__module__, error.__class__.__name__),
+            ],
+        )
         tags.extend(self.system_tags())
 
         report = Report(title=title, content=error_content, tags=tags)
