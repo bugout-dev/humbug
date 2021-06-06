5eff25fe02b9fd678000be22d626b23ef04bb0ec
zomglings <nkashy1@gmail.com>
2021-06-06 12:56:00 -0700

    Bumped humbug Python version to 0.2.3

--- python/setup.py
+++ python/setup.py
@@ -6,7 +6,7 @@ with open("README.md") as ifp:
 
 setup(
     name="humbug",
-    version="0.2.2",
+    version="0.2.3",
     packages=find_packages(),
     install_requires=["requests"],
     extras_require={
